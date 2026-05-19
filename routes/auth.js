0aaa1d656b3af14c2bc1a5549a7979b4fbc5e5bd
RelaX <relaxbilardo@gmail.com>
2026-05-19 14:22:20 +0300

    fix zodiac save and profile update logs

--- routes/auth.js
+++ routes/auth.js
@@ -357,8 +357,64 @@ router.post("/update-profile", async (req, res) => {
     user.surname = surname;
     user.birthDate = birthDate;
     user.birthTime = birthTime;
-    user.zodiac = zodiac;
-    user.isProfileCompleted = true;
+
+const month =
+new Date(birthDate).getMonth() + 1;
+
+const day =
+new Date(birthDate).getDate();
+
+let autoZodiac = "";
+
+if ((month == 3 && day >= 21) || (month == 4 && day <= 19))
+autoZodiac = "Koç";
+
+else if ((month == 4 && day >= 20) || (month == 5 && day <= 20))
+autoZodiac = "Boğa";
+
+else if ((month == 5 && day >= 21) || (month == 6 && day <= 20))
+autoZodiac = "İkizler";
+
+else if ((month == 6 && day >= 21) || (month == 7 && day <= 22))
+autoZodiac = "Yengeç";
+
+else if ((month == 7 && day >= 23) || (month == 8 && day <= 22))
+autoZodiac = "Aslan";
+
+else if ((month == 8 && day >= 23) || (month == 9 && day <= 22))
+autoZodiac = "Başak";
+
+else if ((month == 9 && day >= 23) || (month == 10 && day <= 22))
+autoZodiac = "Terazi";
+
+else if ((month == 10 && day >= 23) || (month == 11 && day <= 21))
+autoZodiac = "Akrep";
+
+else if ((month == 11 && day >= 22) || (month == 12 && day <= 21))
+autoZodiac = "Yay";
+
+else if ((month == 12 && day >= 22) || (month == 1 && day <= 19))
+autoZodiac = "Oğlak";
+
+else if ((month == 1 && day >= 20) || (month == 2 && day <= 18))
+autoZodiac = "Kova";
+
+else
+autoZodiac = "Balık";
+
+user.zodiac = autoZodiac;
+console.log(
+"BURÇ KAYDEDİLİYOR:",
+autoZodiac
+);
+
+console.log(
+"FINAL USER:",
+user
+);
+
+
+user.isProfileCompleted = true;
 
     await user.save();
 
