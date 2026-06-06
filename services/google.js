e44a9846856829fb8a9562a476071683c911544c
RelaX <relaxbilardo@gmail.com>
2026-06-06 19:02:38 +0300

    Use Railway Google service account

--- services/google.js
+++ services/google.js
@@ -1,7 +1,11 @@
 const { google } = require("googleapis");
 
+const serviceAccount = JSON.parse(
+  process.env.GOOGLE_SERVICE_ACCOUNT
+);
+
 const auth = new google.auth.GoogleAuth({
-  keyFile: "google-service-account.json",
+  credentials: serviceAccount,
   scopes: ["https://www.googleapis.com/auth/androidpublisher"],
 });
 
@@ -13,8 +17,8 @@ const androidpublisher = google.androidpublisher({
 const verifyPayment = async (token, productId) => {
   const res = await androidpublisher.purchases.products.get({
     packageName: "com.mysticfal.mobile",
-    productId: productId,
-    token: token,
+    productId,
+    token,
   });
 
   return res.data;
