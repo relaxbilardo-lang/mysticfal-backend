92a67852ae2ed697098781f31529406ea4cf0700
RelaX <relaxbilardo@gmail.com>
2026-06-08 12:11:53 +0300

    Add change password debug logs

--- routes/auth.js
+++ routes/auth.js
@@ -347,6 +347,7 @@ router.post("/verify-otp", async (req, res) => {
   // ================= CHANGE PASSWORD =================
 router.post("/change-password", async (req, res) => {
   console.log("🚨 CHANGE PASSWORD ROUTE HIT");
+  console.log("🚨 BODY:", req.body);
   try {
     const {
       userId,
